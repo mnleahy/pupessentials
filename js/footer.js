a605f0c3f6ddd529ea05c0cca088a1ad4e7f15d3
mnleahy <116642982+mnleahy@users.noreply.github.com>
2022-12-09 10:57:05 +0000

    Update footer.js

--- js/footer.js
+++ js/footer.js
@@ -3,8 +3,8 @@ const footertemplate = document.createElement('template')
 footertemplate.innerHTML=`
 <div class="ps-5 pe-5 dpink">
     <footer class="row d-flex flex-wrap justify-content-between align-items-center py-3">
-      <p class="col-md-4 mb-0 text-light ctr">&copy; 2022 Company, Inc</p>
-      <ul class="nav col-md-4 justify-content-end">
+      <p class="col-md-6 mb-0 text-light text-center">&copy; 2022 Pup Essentials Limited</p>
+      <ul class="nav col-md-6 justify-content-center">
         <li class="nav-item"><a href="index.html" class="nav-link px-2 text-light">Home</a></li>
         <li class="nav-item"><a href="about.html" class="nav-link px-2 text-light">About</a></li>
         <li class="nav-item"><a href="services.html" class="nav-link px-2 text-light">Services</a></li>
