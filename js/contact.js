38d13dab339af0de38e77fbaf317ec0713f90038
CloByrne <116455522+CloByrne@users.noreply.github.com>
2022-12-11 09:49:02 +0000

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,9 @@
-/* JS by Clodagh Byrne*/
-
+/*JS by Clodagh Byrne*/
+//Message on clicking submit
 function alter() {
 	// variables and response message
 	var name = document.getElementById("myName").value;
-	var email = document.getElementById("myEmail").value;
+	var email = document.getElementById("email").value;
 	var message = document.getElementById("message").value;
 	if(name!="" && email!="" && message!=""){
 		document.getElementById("name").innerHTML = name+", thank you for your details. We will be in touch via "+email+" shortly";
@@ -13,19 +13,21 @@ function alter() {
 		document.getElementById("name").innerHTML ="Please fill in all fields of the form before submitting";
 	}
 }
-	// hide the form after submission
-  function hide() {
-  document.getElementById("hideform").style.display="none";
-  }
 
-  // Check char limit
-  if (fname.length > 50) {
-        alert("The name may have no more than 50 characters");
-        submitOK = "false";
-  }
-
-  // Check valid email
-  if (at === -1) {
-      alert("Not a valid email address!");
-      submitOK = "false";
-  }
+//Email address validation
+function email_validation(){
+	'use strict';
+	var mailformat = /^\w+([\.\-]?\w+)*@\w+([\.\-]?\w+)*(\.\w{2,3})+$/;
+	var email_name = document.getElementById("email");
+	var email_value = document.getElementById("email").value;
+	var email_length = email_value.length;
+	if(!email_value.match(mailformat) || email_length === 0){
+		document.getElementById('email_err').innerHTML = 'This is not a valid email format.';
+		email_name.focus();
+		document.getElementById('email_err').style.color = "#F3071C";
+	}
+	else{
+		document.getElementById('email_err').innerHTML = 'Valid email format';
+		document.getElementById('email_err').style.color = "#19B438";
+	}
+}
